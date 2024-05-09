fd3a59d537b027fa72c53755f24a27659d316602
OscarSalas19 <oscar.salas.fallas@est.una.ac.cr>
2024-05-08 19:51:53 -0600

    practice of making a commit

--- source.cpp
+++ source.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
 using std:cout;
 using std:endl;
+
+
+int suma1(int num1, int num2) {
+	return (num1 + num2);
+}
 int main() {
+	int suma = 0;
+	suma = suma1(3, 5);
+
+	cout << suma << endl;
+
+
 
 	cout << "Boomboclat" << endl;
+
+
 
 	return 0;
 }
